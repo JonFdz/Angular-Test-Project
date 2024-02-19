8f18a5383175310591b9adddd01b51acf5c76adf
Jon Fernandez <jfernandez@nechigroup.com>
2024-02-19 18:11:35 +0100

    Updated server.component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 export class ServerComponent {
     serverId = 10;
     serverStatus = 'offline';
+
     getServerStatus() {
         return this.serverStatus;
     }
