ee7c44a64528319e4e66559f91862938ba5cd8f2
JonFdz <jonfernal@gmail.com>
2024-03-18 11:42:59 +0100

    Observables Start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,22 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { UsersComponent } from "./users/users.component";
-import { HomeComponent } from "./home/home.component";
-import { UserComponent } from "./users/user/user.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { EditServerComponent } from "./servers/edit-server/edit-server.component";
-import { ServerComponent } from "./servers/server/server.component";
-import { ServersComponent } from "./servers/servers.component";
-import { AuthGuard } from "./auth-guard.service";
-import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate.service";
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
-const appRoutes: Routes = [
-	{ path: '', component: HomeComponent },
-	{
-		path: 'users', component: UsersComponent, children: [
-			{ path: ':id/:name', component: UserComponent }
-		]
-	},
-	{
-		path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
-			{ path: ':id', component: ServerComponent },
-			{ path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}
-		]
-	},
-	{ path: 'not-found', component: PageNotFoundComponent },
-	{ path: '**', redirectTo: '/not-found' }
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'user/:id', component: UserComponent}
 ];
 
 @NgModule({
-	imports: [
-		RouterModule.forRoot(appRoutes)
-	],
-	exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
