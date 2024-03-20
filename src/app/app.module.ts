736843363a18a77a468f929be4dc8a262dd2b2db
Jon <jonfernal@gmail.com>
2024-03-20 10:46:54 +0100

    Reactive Forms Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
